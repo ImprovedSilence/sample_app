a81d930bfacc92adf2dff4330655f0fd5b7827c2
Shawn Henderson <shawn12@gmail.com>
2012-03-08 22:13:46 -0500

    finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+	describe "Home page" do
+		it "should have the content 'Sample App'" do
+			visit '/static_pages/home'
+			page.should have_content('Sample App')
+		end
+		it "should have the rigth title" do
+			visit '/static_pages/home'
+			page.should have_selector('title',
+											:text => "Ruby on Rails Tutorial Sample App | Home")
+		end
+	end
+
+	describe "help pages" do
+		it "should have conetnt 'Help'" do
+			visit '/static_pages/help'
+			page.should have_content('Help')
+		end
+		it "should have the rigth title" do
+			visit '/static_pages/help'
+			page.should have_selector('title',
+											:text => "Ruby on Rails Tutorial Sample App | Help")
+		end
+	end
+
+	describe "About pages" do
+		it "should have the conetent 'About us'" do
+			visit '/static_pages/about'
+			page.should have_content('About Us')
+		end
+		it "should have the rigth title" do
+			visit '/static_pages/about'
+			page.should have_selector('title',
+											:text => "Ruby on Rails Tutorial Sample App | About")
+		end
+	end
+end
