e8c86f16510a095d1cec2df4e3c2ad27042b1617
Shawn Henderson <shawn12@gmail.com>
2012-03-28 20:09:15 -0400

    fix current_user? prob (it's in the user_con -> edit)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,7 @@ class UsersController < ApplicationController
 	end
 
 	def edit
+		@user = User.find(params[:id])
 	end
 
 	def update 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,14 +15,14 @@ module SessionsHelper
 	end
 
 	def current_user=(user)
-		@current_user=user
+		@current_user = user
 	end
 
 	def current_user
 		@current_user ||= user_from_remember_token
 	end
 
-	def current_user?
+	def current_user?(user)
 		user == current_user
 	end
 
